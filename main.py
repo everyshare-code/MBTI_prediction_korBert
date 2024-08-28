1736d53f571f178f6775b750382dde43a1cdcdf0
everyshare-code <138933190+everyshare-code@users.noreply.github.com>
2024-08-29 07:26:16 +0900

    테스트 서버 포트 변경

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from app import app #플라스크 어플리케이션
 from asgiref.wsgi import WsgiToAsgi
 import uvicorn
+from dotenv import load_dotenv
 
-
+load_dotenv()
 asgi_app = WsgiToAsgi(app)#플라스크 앱을 ASGI와 호환되는 WSGI 앱으로 변환
 
 if __name__ == '__main__':
-    uvicorn.run(asgi_app,host='0.0.0.0',port=5000)
+    uvicorn.run(asgi_app, host='0.0.0.0', port=5001)
